7b2e4e76256aa1ca8cfc3f3a7d51e90a4a0f276e
Guilherme Bezerra <guilherme.bezerra@esig.com.br>
2022-12-18 09:16:21 -0300

    Configuração Inicial do Projeto

---
+++ src/main/java/br/com/listaDeComprasApi/ListaDeComprasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.listaDeComprasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeComprasApiApplication.class, args);
+	}
+
+}
